98520a99a953d88c74e6b43730c3bd39d0606d8b
Martin Bříza <m@rtinbriza.cz>
2016-08-08 18:21:03 +0200

    Make the power of 1000 constants UL literals

--- app/drivemanager.cpp
+++ app/drivemanager.cpp
@@ -160,19 +160,19 @@ Progress *Drive::progress() const {
 
 QString Drive::name() const {
     QString sizeStr;
-    if (m_size < (1000)) {
+    if (m_size < (1000UL)) {
         sizeStr = QString(" (%1 B)").arg(m_size);
     }
-    else if (m_size < (1000L*1000)) {
+    else if (m_size < (1000UL*1000)) {
         sizeStr = QString(" (%1 KB)").arg(m_size / 1000.0, 0, 'f', 1);
     }
-    else if (m_size < (1000L*1000*1000)) {
+    else if (m_size < (1000UL*1000*1000)) {
         sizeStr = QString(" (%1 MB)").arg(m_size / 1000000.0, 0, 'f', 1);
     }
-    else if (m_size < (1000L*1000*1000*1000)) {
+    else if (m_size < (1000UL*1000*1000*1000)) {
         sizeStr = QString(" (%1 GB)").arg(m_size / 1000000000.0, 0, 'f', 1);
     }
-    else if (m_size < (1000L*1000*1000*1000*1000)) {
+    else if (m_size < (1000UL*1000*1000*1000*1000)) {
         sizeStr = QString(" (%1 TB)").arg(m_size / 1000000000000.0, 0, 'f', 1);
     }
     else { // better be ready for exabyte images and drives
