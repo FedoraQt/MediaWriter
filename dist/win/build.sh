ea5b7f0666f2f5ac8f255ea298e1b046602c2e94
Martin Briza <mbriza@redhat.com>
2016-08-26 14:52:51 +0200

    Add scripts for code signing

--- dist/win/build.sh
+++ dist/win/build.sh
@@ -10,7 +10,15 @@ BUILDPATH="$ROOTPATH/build"
 MEDIAWRITER="$BUILDPATH/app/release/mediawriter.exe"
 HELPER="$BUILDPATH/app/helper.exe"
 
-PACKAGES="mingw32-mediawriter mingw32-qt5-qtbase mingw32-qt5-qtdeclarative mingw32-qt5-qtquickcontrols mingw32-nsis"
+if [ -z "$CERTPATH" ]; then
+    CERTPATH=~
+fi
+if [ -z "$CERTPASS" ]; then
+    echo "You have not provided your certificate passphrase."
+    exit 1
+fi
+
+PACKAGES="mingw32-mediawriter mingw32-qt5-qtbase mingw32-qt5-qtdeclarative mingw32-qt5-qtquickcontrols mingw32-nsis mono-devel"
 
 BINARIES="libstdc++-6.dll libwinpthread-1.dll libgcc_s_sjlj-1.dll libpng16-16.dll libharfbuzz-0.dll libpcre-1.dll libintl-8.dll iconv.dll libpcre16-0.dll libEGL.dll libglib-2.0-0.dll libGLESv2.dll zlib1.dll Qt5Core.dll Qt5Gui.dll Qt5Network.dll Qt5Qml.dll Qt5Quick.dll Qt5Svg.dll Qt5Widgets.dll Qt5WinExtras.dll"
 PLUGINS="imageformats/qjpeg.dll imageformats/qsvg.dll platforms/qwindows.dll"
@@ -67,10 +75,20 @@ cp "$HELPER" .
 
 #zip -r mediawriter.zip * >/dev/null
 
+# See http://stackoverflow.com/questions/18287960/signing-windows-application-on-linux-based-distros for details
+echo "=== Signing binaries"
+signcode  -spc $CERTPATH/authenticode.spc -v $CERTPATH/authenticode.pvk -a sha1 -$ commercial -n "Fedora Media Writer" -i https://getfedora.org -t http://timestamp.verisign.com/scripts/timstamp.dll -tr 10 $MEDIAWRITER >/dev/null <<< "$CERTPASS"
+rm "$MEDIAWRITER.bak"
+signcode  -spc $CERTPATH/authenticode.spc -v $CERTPATH/authenticode.pvk -a sha1 -$ commercial -n "Fedora Media Writer" -i https://getfedora.org -t http://timestamp.verisign.com/scripts/timstamp.dll -tr 10 $HELPER >/dev/null <<< "$CERTPASS"
+rm "$HELPER.bak"
+
 popd >/dev/null
 popd >/dev/null
 
 echo "=== Composing installer"
 makensis "$SCRIPTDIR/mediawriter.nsi" >/dev/null
+
+signcode  -spc $CERTPATH/authenticode.spc -v $CERTPATH/authenticode.pvk -a sha1 -$ commercial -n "Fedora Media Writer" -i https://getfedora.org -t http://timestamp.verisign.com/scripts/timstamp.dll -tr 10 "$SCRIPTDIR/FMW-setup.exe" >/dev/null <<< "$CERTPASS"
+rm "$SCRIPTDIR/FMW-setup.exe.bak"
 
 echo "=== Installer is located in $SCRIPTDIR/FMW-setup.exe"
