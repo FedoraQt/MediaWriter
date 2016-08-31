4b7507c6706f7f736c11117c9f21bc6ed2181bf3
Martin Briza <mbriza@redhat.com>
2016-08-31 15:52:38 +0200

    Make the app packageable with Flatpak
    Doesn't work right so far though :(

--- dist/flatpak/build.sh
+++ dist/flatpak/build.sh
@@ -5,5 +5,6 @@ FILE=$1
 APPID=`basename $FILE .json`
 
 echo ========== Building $APPID ================
-flatpak-builder --force-clean --ccache --require-changes --repo=repo --subject="Nightly build of ${APPID}, `date`" ${EXPORT_ARGS-} app $FILE
+# --require-changes
+flatpak-builder --disable-cache --force-clean --ccache --repo=repo --subject="Nightly build of ${APPID}, `date`" ${EXPORT_ARGS-} app $FILE
 
