3cbb96ed6987a2d0d68b461ebdd27c4a3361dcd4
Martin Briza <mbriza@redhat.com>
2016-09-20 16:22:36 +0200

    Improve the translations pulling script and fix its result

--- translations/pull.sh
+++ translations/pull.sh
@@ -7,8 +7,9 @@ zanata-cli --batch-mode pull
 echo -e '<RCC>' > translations.qrc
 echo -e '\t<qresource prefix="/translations/">' >> translations.qrc
 for i in `ls *.po`; do
+	sed -i '/"Language:.*/a "X-Qt-Contexts: true\\n"' $i
 	lrelease-qt5 $i
-	echo -e "\t\t<file>$i</file>" >> translations.qrc
+	echo -e "\t\t<file>${i/.po/.qm}</file>" >> translations.qrc
 done
 echo -e '\t</qresource>' >> translations.qrc
 echo -e '</RCC>' >> translations.qrc
