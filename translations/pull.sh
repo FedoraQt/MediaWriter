c99cab641ec50303766dba59ef87fedb5862d7e2
Martin Briza <mbriza@redhat.com>
2016-09-26 16:02:45 +0200

    Translations in desktop and appstream files

--- translations/pull.sh
+++ translations/pull.sh
@@ -6,10 +6,33 @@ zanata-cli --batch-mode pull
 
 echo -e '<RCC>' > translations.qrc
 echo -e '\t<qresource prefix="/translations/">' >> translations.qrc
-for i in `ls *.po`; do
+for i in `ls mediawriter-*.po`; do
 	sed -i '/"Language:.*/a "X-Qt-Contexts: true\\n"' $i
-	lrelease-qt5 $i
-	echo -e "\t\t<file>${i/.po/.qm}</file>" >> translations.qrc
+	LANGCODE=$(sed 's/mediawriter-\([^.]*\).po/\1/' <<< "$i")
+	lrelease-qt5 $i -qm $LANGCODE.qm
+	echo -e "\t\t<file>${LANGCODE}.qm</file>" >> translations.qrc
 done
 echo -e '\t</qresource>' >> translations.qrc
 echo -e '</RCC>' >> translations.qrc
+
+rm mediawriter-*.po
+
+for i in `ls appstream-*.po`; do
+    echo $i
+    LANGCODE=$(sed 's/appstream-\([^.]*\).po/\1/' <<< "$i")
+    msgfmt $i -o "${LANGCODE}.mo"
+done
+
+itstool -i as-metainfo.its -j ../dist/linux/mediawriter.appdata.xml.in -o ../dist/linux/mediawriter.appdata.xml *.mo
+
+rm appstream-*.po *.mo
+
+for i in `ls desktop-*.po`; do
+    echo $i
+    LANGCODE=$(sed 's/desktop-\([^.]*\).po/\1/' <<< "$i")
+    mv $i "$LANGCODE.po"
+done
+
+intltool-merge -d . ../dist/linux/mediawriter.desktop.in ../dist/linux/mediawriter.desktop
+
+rm *.po
