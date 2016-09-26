ad7ef59ed28bd967a9d678b335c90699a8ca041e
Martin Briza <mbriza@redhat.com>
2016-09-26 15:00:15 +0200

    Allow translating appstream and desktop metadata
    Technically fixes #17 :) However, I have yet to figure out how to actually use the translations

--- translations/push.sh
+++ translations/push.sh
@@ -3,4 +3,6 @@
 rm -f mediawriter.pot mediawriter.ts
 lupdate-qt5 ../mediawriter.pro -ts mediawriter.ts
 lconvert-qt5 -of po -o mediawriter.pot mediawriter.ts
+xgettext ../dist/linux/mediawriter.desktop -o desktop.pot
+itstool -i as-metainfo.its -o appstream.pot ../dist/linux/mediawriter.appdata.xml 
 zanata-cli push -B
