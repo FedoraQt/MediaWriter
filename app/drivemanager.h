0de6cfe8470f55386aafacfa5d325531c13b1fac
Martin Briza <mbriza@redhat.com>
2016-11-07 19:35:18 +0100

    Mark roleNames as override

--- app/drivemanager.h
+++ app/drivemanager.h
@@ -42,7 +42,7 @@ public:
     explicit DriveManager(QObject *parent = 0);
 
     QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
-    QHash<int, QByteArray> roleNames() const;
+    QHash<int, QByteArray> roleNames() const override;
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
 
