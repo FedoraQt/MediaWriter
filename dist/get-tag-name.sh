c374d745a4752ac1999a181bfff4d1795912ee13
Martin Bříza <m@rtinbriza.cz>
2020-07-02 12:39:49 +0200

    Allow dash-something in the tag string so we can do rebuilds

--- dist/get-tag-name.sh
+++ dist/get-tag-name.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 PROBABLY_TAG=${GITHUB_REF/refs\/tags\//}
+REGEXP="[0-9][0-9]*[.][0-9][0-9]*[.][0-9][0-9]*"
 
-grep "^[0-9][0-9]*[.][0-9][0-9]*[.][0-9][0-9]*$" <<< "$PROBABLY_TAG"
+grep "$REGEXP" <<< "$PROBABLY_TAG" | sed "s/.*\($REGEXP\).*/\\1/"
 
 exit 0
