28d29cb47b537d915250ad3d817feb2a3878215e
Jan Grulich <jgrulich@redhat.com>
2022-03-05 22:00:07 +0100

    Update translation script and pot file

--- po/generate-pot-files.sh
+++ po/generate-pot-files.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 rm -f mediawriter.pot mediawriter.ts
-lupdate-qt5 ../mediawriter.pro -ts mediawriter.ts
-lconvert-qt5 -of po -o app.pot mediawriter.ts
-xgettext ../dist/linux/org.fedoraproject.MediaWriter.desktop -o desktop.pot
-itstool -i as-metainfo.its -o appstream.pot ../dist/linux/org.fedoraproject.MediaWriter.appdata.xml.in
-json2po -i ../app/assets/metadata.json -o metadata.pot
+lupdate-qt6 ../src/app/qml.qrc ../src//app/*.cpp -ts mediawriter.ts
+lconvert-qt6 -of po -o app.pot mediawriter.ts
+xgettext ../src/app/data/org.fedoraproject.MediaWriter.desktop -o desktop.pot
+itstool -i as-metainfo.its -o appstream.pot ../src/app/data/org.fedoraproject.MediaWriter.appdata.xml.in
 msgcat *.pot > mediawriter.pot
-rm app.pot appstream.pot desktop.pot metadata.pot
+rm app.pot appstream.pot desktop.pot
