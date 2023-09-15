846672350f334729955389c8fa046f6a6882c29d
Rafael Fontenelle <rffontenelle@users.noreply.github.com>
2023-09-15 13:58:31 -0300

    Translations: change run directories in generate-pot-files.sh

--- po/generate-pot-files.sh
+++ po/generate-pot-files.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-rm -f mediawriter.pot mediawriter.ts
-lupdate-qt6 ../src/app/qml.qrc ../src/app/*.cpp ../src/app/*.h ../src/helper/linux/*.cpp ../src/helper/mac/*.cpp ../src/helper/win/*.cpp -ts mediawriter.ts
-lconvert-qt6 -of po -o app.pot mediawriter.ts
-xgettext ../src/app/data/org.fedoraproject.MediaWriter.desktop -o desktop.pot
-itstool -i as-metainfo.its -o appstream.pot ../src/app/data/org.fedoraproject.MediaWriter.appdata.xml.in
-msgcat *.pot > mediawriter.pot
-rm app.pot appstream.pot desktop.pot
+cd $(dirname $0)/..
+rm -f po/mediawriter.pot mediawriter.ts
+lupdate-qt6 src/app/qml.qrc src/app/*.cpp src/app/*.h src/helper/linux/*.cpp src/helper/mac/*.cpp src/helper/win/*.cpp -ts mediawriter.ts
+lconvert-qt6 -of po -o po/app.pot mediawriter.ts
+xgettext src/app/data/org.fedoraproject.MediaWriter.desktop -o po/desktop.pot
+itstool -i po/as-metainfo.its -o po/appstream.pot src/app/data/org.fedoraproject.MediaWriter.appdata.xml.in
+msgcat po/*.pot > po/mediawriter.pot
+rm po/app.pot po/appstream.pot po/desktop.pot
