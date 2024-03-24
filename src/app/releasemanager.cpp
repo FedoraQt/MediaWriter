40aabd25690f3de653cca6022456b23420ca6cdf
eatradish <sakiiily@aosc.io>
2024-03-24 13:42:09 +0800

    fix: fix a typo

--- src/app/releasemanager.cpp
+++ src/app/releasemanager.cpp
@@ -1049,7 +1049,7 @@ void ReleaseVariant::setErrorString(const QString &o)
 ReleaseArchitecture ReleaseArchitecture::m_all[] = {
     {{"amd64"}, QT_TR_NOOP("Intel/AMD 64bit"), QT_TR_NOOP("ISO format image for Intel, AMD and other compatible PCs (64-bit)")},
     {{"arm64"}, QT_TR_NOOP("AArch64"), QT_TR_NOOP("ISO format image for AArch64 machine")},
-    {{"loongarch64"}, QT_TR_NOOP("AArch64"), QT_TR_NOOP("ISO format image for LoongArch64 machine")},
+    {{"loongarch64"}, QT_TR_NOOP("LoongArch64"), QT_TR_NOOP("ISO format image for LoongArch64 machine")},
     {{"ppc64el"}, QT_TR_NOOP("POWER"), QT_TR_NOOP("ISO format image for POWER machine")},
     {{"loongson3"}, QT_TR_NOOP("Loongson 3"), QT_TR_NOOP("ISO format image for Loongson 3 machine")},
     {{"riscv64"}, QT_TR_NOOP("RISCV 64bit"), QT_TR_NOOP("ISO format image for RISCV-64bit machine")},
